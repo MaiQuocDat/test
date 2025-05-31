b94272a31e1b8746c4e930f9c43cc6a27e2f4229
MaiQuocDat <narkisus1995@gmail.com>
2025-05-31 20:58:28 +0700

    add file

---
+++ c2405l1_giuaky_group03-laravel-sanctum/routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\RecipePrepareController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+
+// Route::get('/users', [UserController::class, 'index'])->name('users.index');
+
+// Route::get('/recipe', [RecipePrepareController::class, 'index'])->name('recipe.index');
+
+// Route::get('/', function () {
+//     return ['Laravel' => app()->version()];
+// });
